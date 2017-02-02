7d37c0097bf50a8c2400bac7d8269dd5bf65b0b4
Student6 <Student6@10.8.33.161>
2017-02-02 12:21:54 -0500

    Made a second file

---
+++ Casino/src/bingo/MakinoonSecondPackage.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package bingo;
+
+/**
+ * @author Makinoon
+ *
+ */
+public class MakinoonSecondPackage {
+
+	/**
+	 * 
+	 */
+	public MakinoonSecondPackage() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
