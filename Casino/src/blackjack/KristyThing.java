3c1ce4ffd6ddadc877249451675032fa7594fe6b
Student06 <Student06@10.8.33.104>
2017-02-02 12:20:50 -0500

    created kristy file

---
+++ Casino/src/blackjack/KristyThing.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package blackjack;
+
+/**
+ * @author Student06
+ *
+ */
+public class KristyThing {
+
+	/**
+	 * 
+	 */
+	public KristyThing() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
