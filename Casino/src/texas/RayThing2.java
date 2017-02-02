203ad5d94fc74534c662e15bb31dbbe30753272a
Student 6 <Student 6@10.8.33.147>
2017-02-02 12:21:51 -0500

    class2

---
+++ Casino/src/texas/RayThing2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package texas;
+
+/**
+ * @author Ray
+ *
+ */
+public class RayThing2 {
+
+	/**
+	 * 
+	 */
+	public RayThing2() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
