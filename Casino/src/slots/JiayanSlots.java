c2b15d0b0a0cd6fb62b55c7ce2598edd796c0df5
Student 6 <Student 6@10.8.33.178>
2017-02-02 12:20:08 -0500

    created a jiayan file

---
+++ Casino/src/slots/JiayanSlots.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package slots;
+
+/**
+ * @author Jiayan
+ *
+ */
+public class JiayanSlots {
+
+	/**
+	 * 
+	 */
+	public JiayanSlots() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
