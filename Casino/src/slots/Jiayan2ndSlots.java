ba51003c217a950fa115a52e89dcb3257cc39137
Student 6 <Student 6@10.8.33.178>
2017-02-02 12:22:04 -0500

    made 2nd jiayan file

---
+++ Casino/src/slots/Jiayan2ndSlots.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package slots;
+
+/**
+ * @author Student 6
+ *
+ */
+public class Jiayan2ndSlots {
+
+	/**
+	 * 
+	 */
+	public Jiayan2ndSlots() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
