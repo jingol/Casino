3461dc9a8e6e868409bac7aac05edf685ac6a87a
Student06 <Student06@10.8.33.104>
2017-02-07 13:05:32 -0500

    created blackjack screen

---
+++ Casino/src/blackjack/BlackjackScreen.java
@@ -0,0 +1,21 @@
+package blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gui.components.Visible;
+import gui.screens.ClickableScreen;
+
+
+public class BlackjackScreen extends ClickableScreen {
+
+	public BlackjackScreen(int width, int height) {
+		super(width, height);
+	}
+
+	@Override
+	public void initAllObjects(ArrayList<Visible> arg0) {
+		
+	}
+
+}
---
+++ Casino/src/blackjack/Dealer.java
@@ -0,0 +1,27 @@
+package blackjack;
+
+/**
+ * @author Kristy Tan
+ *
+ */
+public class Dealer {
+
+	
+	public Dealer() {
+		
+	}
+	
+	//check 21 value
+	public void checkValue(){
+		
+	}
+	
+	//if 17 or less, dealer must hit
+	public void hit(){
+		
+	}
+	//else stand
+	public void stay(){
+		
+	}
+}
