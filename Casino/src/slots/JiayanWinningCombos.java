ebf45e63656a349c214889101d1036ef35940b5e
Student 6 <Student 6@10.8.33.178>
2017-02-08 12:26:11 -0500

    initialized necessary fields and methods

---
+++ Casino/src/slots/JiayanWinningCombos.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package slots;
+
+import java.util.ArrayList;
+
+/**
+ * @author Jiayan
+ *
+ */
+public class JiayanWinningCombos {
+	//public ArrayList<>imageList = new ArrayList<>();
+	public int [] reward = new int [6];
+	private int betAmount;
+	//public ArrayList<Reward>RewardHistory = new ArrayList<Reward>(RewardHistory);
+	//
+	/**
+	 * use constants to store value of a slot image?
+	 * rewards generated from slot images produced by Jason
+	 * multiple wins
+	 * rewards vary depending on nockles' faces
+	 * some faces appear more often compared to others
+	 * 
+	 */
+	public JiayanWinningCombos() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void checkHorizontal()
+	{
+		
+	}
+	public static void checkVertical()
+	{
+		
+	}
+	public static void checkDiagonal()
+	{
+		
+	}
+}
