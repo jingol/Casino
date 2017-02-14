39f1569b67bfe98f90bf0ba5855043a10224404a
David Szymanski <dszymanski1408@bths.edu>
2017-02-13 20:24:35 -0500

    Update BlackjackScreen.java

--- Casino/src/blackjack/BlackjackScreen.java
+++ Casino/src/blackjack/BlackjackScreen.java
@@ -7,18 +7,48 @@ import java.util.List;
 
 import gui.components.Visible;
 import gui.screens.ClickableScreen;
+import gui.components.TextArea;
 
 
 public class BlackjackScreen extends ClickableScreen {
 
+	private TextArea score;
+	private ArrayList<CardInterface> hand; 
+	private ButtonInterface[] button;
+	
 	public BlackjackScreen(int width, int height) {
 		super(width, height);
+		Thread bj = new Thread(this);
+		bj.start();
 	}
 
 	@Override
-	public void initAllObjects(ArrayList<Visible> arg0) {
-
+	public void initAllObjects(ArrayList<Visible> viewObjects) {
+		addButtons(viewObjects);
+		for(int i = 0; i < 4; i++){
+			viewObjects.add(button[i]);
+		}
+		hand = new ArrayList<CardInterface>();
+		score = new TextArea(130, 230, 300, 40, getScore());
+		hand.add(drawCard());
+		hand.add(drawCard());
 	}
+	
+		
+	public CardInterface drawCard(){
+		return null;
+	}
+	
+	public String getScore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addButtons(ArrayList<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 	public void drawBackground(Graphics2D g){
 		g.setColor(Color.green);
