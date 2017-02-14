d19d0234de5d890e9f0f1b75d2832bfb1d59b152
Student6 <Student6@10.8.33.97>
2017-02-14 11:57:12 -0500

    xdxd

--- Casino/src/slots/SlotScreen.java
+++ Casino/src/slots/SlotScreen.java
@@ -2,21 +2,31 @@ package slots;
 
 import java.util.ArrayList;
 
+import gui.components.Visible;
 import gui.screens.ClickableScreen;
 
-
-public abstract class SlotScreen extends ClickableScreen implements Runnable {
+public class SlotScreen extends ClickableScreen implements Runnable {
 	
 	public ArrayList<Slotpic> slots;
-	
-	
-	public SlotScreen(int width, int height){
-		super(width,height);
+
+	public SlotScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initAllObjects(ArrayList<Visible> arg0) {
+		// TODO Auto-generated method stub
+
 	}
 	
-	public void run(){
-		
-	}
+	
 	
 	public void spin(){
 		
@@ -25,21 +35,5 @@ public abstract class SlotScreen extends ClickableScreen implements Runnable {
 	public void update(){
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	//----- 
-	//  |   
-	//    
-	//     
-	//     
-	
-	
-	
-
 
 }
