c272dd3ff5c5e58a48edcc711621945d3100c39b
Sharon <Sharon@DESKTOP-1PNI6LC>
2017-02-15 01:01:02 -0500

    rand num gen

--- Casino/src/bingo/BingoScreenSharon.java
+++ Casino/src/bingo/BingoScreenSharon.java
@@ -6,6 +6,7 @@ package bingo;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import guiCompononets.Visible;
 import guiScreens.ClickableScreen;
@@ -18,10 +19,7 @@ public class BingoScreenSharon extends ClickableScreen implements Runnable{
 public static ArrayList<Integer> randBingoNum;
 private SquaresInterfaceSharonWong[] squares;
 
-	/**
-	 * @param width
-	 * @param height
-	 */
+
 	public BingoScreenSharon(int width, int height) {
 		super(width, height);
 		
@@ -29,9 +27,6 @@ private SquaresInterfaceSharonWong[] squares;
 		bingoBegin.start();
 	}
 
-	/* (non-Javadoc)
-	 * @see guiScreens.ClickableScreen#initAllObjects(java.util.List)
-	 */
 	@Override
 	public void initAllObjects(ArrayList<Visible> square) {
 		addSquares(square);
@@ -44,19 +39,51 @@ private SquaresInterfaceSharonWong[] squares;
 		//place all squares
 				for(int i =0; i < numberOfSquares; i++){
 					squares[i] = getASquare();
-					final SquaresInterfaceSharonWong b = squares[i];
+					//final SquaresInterfaceSharonWong b = squares[i];
 				
 				}
 				
 	}
 
-	/* (non-Javadoc)
-	 * @see gui.Screen#initObjects(java.util.ArrayList)
-	 */
 	@Override
 	public void initObjects(ArrayList<Visible> arg0) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public  void playerBoardArray() {
+		Random randomGenerator = new Random();
+		while (.length < ) {
+
+		    int random = randomGenerator .nextInt(4);
+		    if (!numbers.contains(random)) {
+		        numbers.add(random);
+		    }
+		}
+		/*
+		//buttons = new ButtonInterfaceSharonWong[5];
+		//code that randomly selects a ButtonInterfaceX
+		int rand = (int)(Math.random()*buttons.length);
+		//if its equal then select rand again
+		while(rand == last){
+			rand = (int) (Math.random()*buttons.length);
+		}
+		last = rand;
+		return new Move(buttons[rand]);
+		*/
+	}
+	
+	// Generate ball spin of random number : used to check if value of button clicked is this number
+	public int randNumBallGenerator(int n)
+	{
+	    ArrayList<Integer> numBallList = new ArrayList<>();
+	    Random random = new Random();
+
+	    for (int i = 0; i < n; i++)
+	    {
+	    	numBallList.add(random.nextInt(25));
+	    }
+	    return numBallList.size()-1;
 	}
 
 	@Override
