9defd7cf248a95ed2bb2d3af132e0b9097195be6
Ray <Ray@192.168.1.107>
2017-02-15 21:58:10 -0500

    oo pile??

---
+++ Casino/src/texas/Pile.java
@@ -0,0 +1,21 @@
+package texas;
+
+import java.awt.Graphics2D;
+
+import gui.components.Component;
+/*
+	Ray
+*/
+public class Pile extends Component {
+
+	public Pile(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		
+	}
+
+	@Override
+	public void update(Graphics2D arg0) {
+		
+	}
+
+}
