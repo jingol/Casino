99184506fbd24af4993283614974b7a4f7c44b5b
Period 6 2.0 <Period 6 2.0@10.8.33.175>
2017-02-17 13:05:48 -0500

    working on boards

--- Casino/src/bingo/BingoScreenSharon.java
+++ Casino/src/bingo/BingoScreenSharon.java
@@ -12,25 +12,27 @@ import guiCompononets.Visible;
 import guiScreens.ClickableScreen;
 
 /**
- * @author Sharon
- *publ asdf
+ * @author Sharon publ asdf
  */
-public class BingoScreenSharon extends ClickableScreen implements Runnable{
-//FIELDS 
-	
-public static ArrayList<Integer> randBingoNum;
-private SquaresInterfaceSharonWong[] squares;
+public class BingoScreenSharon extends ClickableScreen implements Runnable {
+	// FIELDS
 
-//2D array of player board
-public static Integer[][] playerBoard= new Integer[5][5];
-//2D array of AI board
-public static Integer[][] aiBoard= new Integer[5][5];
-public static Boolean[][] aiBoolean= new Boolean[5][5];
-public static Boolean[][] playerBoolean= new Boolean[5][5];
+	public static ArrayList<Integer> spinner;
+	public static Integer randBingoNum;
+	private SquaresInterfaceSharonWong[] squares;
+
+	// 2D array of player board
+	public static Integer[][] playerBoard = new Integer[5][5];
+	// 2D array of AI board
+	public static Integer[][] aiBoard = new Integer[5][5];
+
+	// 2D arrays for checking if clicked, change color
+	public static Boolean[][] aiBoolean = new Boolean[5][5];
+	public static Boolean[][] playerBoolean = new Boolean[5][5];
 
 	public BingoScreenSharon(int width, int height) {
 		super(width, height);
-		
+
 		Thread bingoBegin = new Thread(this);
 		bingoBegin.start();
 	}
@@ -41,119 +43,128 @@ public static Boolean[][] playerBoolean= new Boolean[5][5];
 	}
 
 	private void addSquares(ArrayList<Visible> square) {
-		//create ArrayList of the squares on the bingo board
+		// create ArrayList of the squares on the bingo board
 		int numberOfSquares = 25;
 		squares = new SquaresInterfaceSharonWong[numberOfSquares];
-		//place all squares
-				for(int i =0; i < numberOfSquares; i++){
-					squares[i] = getASquare();
-					
-					//final SquaresInterfaceSharonWong b = squares[i];
-				}			
+		// place all squares
+		for (int i = 0; i < numberOfSquares; i++) {
+			squares[i] = getASquare();
+
+			// final SquaresInterfaceSharonWong b = squares[i];
+		}
 	}
-	
 
 	@Override
 	public void initObjects(ArrayList<Visible> view) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	public int randNumGenerator(){ // generates rand num between 1-50
-		double rand= Math.random();
-		int roll=(int)(51*rand); //0 to 50
-		return roll+1; // 1 to 50
+
+	public int randNumGenerator() { // generates rand num between 1-50
+		double rand = Math.random();
+		int roll = (int) (51 * rand); // 0 to 50
+		return roll + 1; // 1 to 50
 	}
+
+	public void initBoards() {
 	
-	public Integer[][] aiBoardArray() {
-		
-		for (int i = 0; i < aiBoard.length; i++){
-	        for (int j = 0; j < aiBoard[i].length; j++){
-	        	//generate random number
-	        	int random = randNumGenerator();
-	        	//check to see if board already contains that number
-	        	for (int x = 0; i < aiBoard.length; x++) {
-	        	    for (int y = 0; j < aiBoard[i].length; y++) {
-	        	        if (aiBoard[x][y].equals(random)){
-	        	        	randNumGenerator();
-	        	    }
-	        	}
-	        	    aiBoard[i][j] = random;
-	            
-	        	    /*
-	        	     * ArrayList = {1,2,3,4,5,6,7,8,9,10}
-	        	     *for (int i = 0; i < aiBoard.length; i++){
-	        			for (int j = 0; j < aiBoard[i].length; j++){
-	        	     * 		int randGenSize = ArrayList.Size();
-	        	     * 		double rand= Math.random();
-							int roll=(int)(randGenSize*rand);
-	        	     * 		aiBoard[i][j] = ArrayList.get(roll);
-	        	     * 		ArrayList.remove(roll);
-	        	     * }
-	        	     * for (int i = 0; i < aiBoard.length; i++){
-	        			for (int j = 0; j < aiBoard[i].length; j++){
-	        			aiboolean[i][j] = false;
-	        			}
-	        			}
-	        	     */
-	        	    
-	        	}
-	        }
-		
+		// ArrayList<Visible>values = {1,2,3,4,5,6,7,8,9,10};
+		for (int i = 1; i < 51; i++) {
+			spinner.add(i);
 		}
-		return aiBoard;
-		
+
+		for (int i = 0; i < aiBoard.length; i++) {
+			for (int j = 0; j < aiBoard[i].length; j++) {
+				int randGenSize = spinner.size();
+				double rand = Math.random();
+				int roll = (int) (randGenSize * rand);
+				aiBoard[i][j] = spinner.get(roll);
+				spinner.remove(roll);
+			}
+
+		}
+		for (int i = 0; i < aiBoard.length; i++) {
+			for (int j = 0; j < aiBoard[i].length; j++) {
+				aiBoolean[i][j] = false;
+			}
+		}
+
+		for (int i = 1; i < 51; i++) {
+			spinner.add(i);
+		}
+
+		for (int i = 0; i < playerBoard.length; i++) {
+			for (int j = 0; j < playerBoard[i].length; j++) {
+				int randGenSize = spinner.size();
+				double rand = Math.random();
+				int roll = (int) (randGenSize * rand);
+				playerBoard[i][j] = spinner.get(roll);
+				spinner.remove(roll);
+			}
+
+		}
+		for (int i = 0; i < playerBoard.length; i++) {
+			for (int j = 0; j < playerBoard[i].length; j++) {
+				playerBoolean[i][j] = false;
+			}
+		}
 	}
-public Integer[][] playerBoardArray() {
-		
-		for (int i = 0; i < playerBoard.length; i++){
-	        for (int j = 0; j < playerBoard[i].length; j++){
-	        	//generate random number
-	        	int random = randNumGenerator();
-	        	//check to see if board already contains that number
-	        	for (int x = 0; i < playerBoard.length; x++) {
-	        	    for (int y = 0; j < playerBoard[i].length; y++) {
-	        	        if (playerBoard[x][y].equals(random)){
-	        	        	randNumGenerator();
-	        	    }
-	        	}
-	        	playerBoard[i][j] = random;
-	            
-	        	}
-	        }
-		
+
+
+
+	
+/*
+	public Integer[][] playerBoardArray() {
+
+		for (int i = 0; i < playerBoard.length; i++) {
+			for (int j = 0; j < playerBoard[i].length; j++) {
+				// generate random number
+				int random = randNumGenerator();
+				// check to see if board already contains that number
+				for (int x = 0; i < playerBoard.length; x++) {
+					for (int y = 0; j < playerBoard[i].length; y++) {
+						if (playerBoard[x][y].equals(random)) {
+							randNumGenerator();
+						}
+					}
+					playerBoard[i][j] = random;
+
+				}
+			}
+
 		}
 		return playerBoard;
-		
-	}
-	
-	
-	// Generate ball spin of random number : used to check if value of button clicked is this number
-	public int randNumBallGenerator(int n)
-	{
-	    ArrayList<Integer> numBallList = new ArrayList<>();
-	    Random random = new Random();
 
-	    for (int i = 0; i < n; i++)
-	    {
-	    	numBallList.add(random.nextInt(25));
-	    }
-	    return numBallList.size()-1;
+	}
+*/
+	// Generate ball spin of random number : used to check if value of button
+	// clicked is this number
+	
+	public int randNumBallGenerator(int n) {
+		ArrayList<Integer> numBallList = new ArrayList<>();
+		Random random = new Random();
+
+		for (int i = 0; i < n; i++) {
+			numBallList.add(random.nextInt(25));
+		}
+		return numBallList.size() - 1;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	private SquaresInterfaceSharonWong getASquare() {
 		return null;
-		//return new Square();
+		// return new Square();
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
