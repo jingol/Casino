c649d4e84ae421efc8181c53fcb5361a2efa8e0f
Student6 <Student6@10.8.33.97>
2017-02-17 12:50:06 -0500

    added iamges but GLTICHYLMAO

--- Casino/src/slots/SlotScreen.java
+++ Casino/src/slots/SlotScreen.java
@@ -50,7 +50,7 @@ public class SlotScreen extends ClickableScreen implements Runnable {
 		title = new TextLabel(450,20,100,40,"Slots");
 		balance = new TextLabel(10,20,200,40,"Balance:$0");
 		bg = new Graphic(0, 0,1000,800, "resources/back.jpg");
-		button1 = new Button(275,550,100,40,
+		button1 = new Button(275,575,100,40,
 				"STOP",Color.RED,
 				new Action() {
 			
@@ -58,7 +58,7 @@ public class SlotScreen extends ClickableScreen implements Runnable {
 				
 			}
 		});
-		button2 = new Button(425,550,100,40,
+		button2 = new Button(425,575,100,40,
 				"STOP",Color.RED,
 				new Action() {
 			
@@ -66,7 +66,7 @@ public class SlotScreen extends ClickableScreen implements Runnable {
 				
 			}
 		});
-		button3 = new Button(575,550,100,40,
+		button3 = new Button(575,575,100,40,
 				"STOP",Color.RED,
 				new Action() {
 			
@@ -74,6 +74,12 @@ public class SlotScreen extends ClickableScreen implements Runnable {
 				
 			}
 		});
+		addPics();
+		for(Slotpic p : slots){
+			
+			viewObjects.add(p);
+		}
+		moveToBack(bg);
 		viewObjects.add(bg);
 		viewObjects.add(title);
 		viewObjects.add(balance);
@@ -86,6 +92,28 @@ public class SlotScreen extends ClickableScreen implements Runnable {
 	
 	
 	public void spin(){
+		
+	}
+	
+	public void addPics(){
+		ArrayList<Slotpic> demo = new ArrayList<Slotpic>();
+		demo.add(new Slotpic(0,0,100,100,"resources/evilflow.png"));
+		demo.add(new Slotpic(0,0,100,100,"resources/flow.png"));
+		demo.add(new Slotpic(0,0,100,100,"resources/gabe.png"));
+		demo.add(new Slotpic(0,0,100,100,"resources/java.png"));
+		demo.add(new Slotpic(0,0,100,100,"resources/noc1png"));
+		demo.add(new Slotpic(0,0,100,100,"resources/noc2.png"));
+		demo.add(new Slotpic(0,0,100,100,"resources/noc3.png"));
+		int x = 0;
+		int y = 0;
+		for(int i = 0; i < 7; i++){
+			Slotpic temp = demo.get(i);
+			temp.setX(x);
+			temp.setY(y);
+			slots.add(demo.get(i));
+			x += 50;
+			y += 50;
+		}
 		
 	}
 	
