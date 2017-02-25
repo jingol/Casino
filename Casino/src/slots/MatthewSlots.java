5651617de26fac5c382c532e6f11d9fc6e1a7aec
Matt <Matt@DESKTOP-6G7U7JQ>
2017-02-24 19:57:21 -0500

    jeff

--- Casino/src/slots/MatthewSlots.java
+++ Casino/src/slots/MatthewSlots.java
@@ -30,11 +30,13 @@ public class MatthewSlots {
 				gui.GUIApplication.setScreen(texas.Texas.texas);
 			}
 		});
+		
 		blackjackButton = new gui.components.Button(166, 750, 200, 100, "To Blackjack", Color.RED, new Action(){
 			public void act() {
 				gui.GUIApplication.setScreen(blackjack.BlackJackGame.bjs);
 			}
 		});
+		
 		bingoButton = new gui.components.Button(166, 750, 200, 100, "To Bingo", Color.RED, new Action(){
 			public void act() {
 				gui.GUIApplication.setScreen(bingo.MakinoonBingoGame.md);
