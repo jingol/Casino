47f96401d50575a88d433f1cea1a9ee9f64b6d1a
Matt <Matt@DESKTOP-6G7U7JQ>
2017-02-24 21:23:00 -0500

    added to casino

--- Casino/src/main/BalanceInterface.java
+++ Casino/src/main/BalanceInterface.java
@@ -2,7 +2,12 @@ package main;
 
 public interface BalanceInterface {
 
+	/*
+	 * by Matthew Yarmolinsky
+	 */
 	public int getBalance();
 	public int setBalance();
+	
+	public void setFromGame(String game);
 	
 }
