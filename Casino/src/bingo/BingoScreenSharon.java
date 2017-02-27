a2723c7d011497e22b372f05ade885e67b1c8d0a
Sharon <Sharon@DESKTOP-1PNI6LC>
2017-02-26 19:32:10 -0500

    new arrayList for bingo cage(bingo ball spin)

--- Casino/src/bingo/BingoScreenSharon.java
+++ Casino/src/bingo/BingoScreenSharon.java
@@ -12,16 +12,17 @@ import guiCompononets.Visible;
 import guiScreens.ClickableScreen;
 
 /**
- * @author Sharon publ asdf
+ * @author Sharon
  */
 public class BingoScreenSharon extends ClickableScreen implements Runnable {
 	// FIELDS
 
 	public static ArrayList<Integer> spinner;
-	public static Integer randBingoNum;
+	public static ArrayList<Integer> numBallList;
+	//public static Integer randBingoNum;
 	private SquaresInterfaceSharonWong[] squares;
 
-	// 2D array of player board
+	// 2D array of player board to add numbers
 	public static Integer[][] playerBoard = new Integer[5][5];
 	// 2D array of AI board
 	public static Integer[][] aiBoard = new Integer[5][5];
@@ -50,7 +51,6 @@ public class BingoScreenSharon extends ClickableScreen implements Runnable {
 		for (int i = 0; i < numberOfSquares; i++) {
 			squares[i] = getASquare();
 
-			// final SquaresInterfaceSharonWong b = squares[i];
 		}
 	}
 
@@ -59,15 +59,15 @@ public class BingoScreenSharon extends ClickableScreen implements Runnable {
 		// TODO Auto-generated method stub
 
 	}
+	
 	//s=size or length of random
-	public int randNumGenerator(int s) { // generates rand num between 1-50
-		//double rand = Math.random();
-		//int roll = (int) (51 * rand); // 0 to 50
-		//return roll + 1; // 1 to 50
-		double rand = Math.random();
-		int roll = (int) ((s+1) * rand);
-		//return roll + 1;
-		return roll; //NOW::::GENERATES NUM BETWEEN 0-50
+	public int randNumGenerator(int s) { 
+		//random.nextInt(max - min + 1) + min
+	
+		Random rand = new Random();
+		int roll= rand.nextInt(s+1);
+		
+		return roll; //if s=50, this GENERATES NUM BETWEEN 0-50
 	}
 
 	public void initBoards() {
@@ -79,7 +79,7 @@ public class BingoScreenSharon extends ClickableScreen implements Runnable {
 		for (int i = 0; i < aiBoard.length; i++) {
 			for (int j = 0; j < aiBoard[i].length; j++) {
 				int randGenSize = spinner.size();
-				int roll = randNumGenerator(randGenSize); // get a random index from current length to choose from
+				int roll = randNumGenerator(randGenSize-1); // get a random index from current length to choose from
 				
 				aiBoard[i][j] = spinner.get(roll);
 				spinner.remove(roll);
@@ -87,27 +87,6 @@ public class BingoScreenSharon extends ClickableScreen implements Runnable {
 
 		}
 		
-		
-	/*
-		
-		// ArrayList<Visible>values = {1,2,3,4,5,6,7,8,9,10};
-		for (int i = 1; i < 51; i++) {
-			spinner.add(i);
-		}
-
-		for (int i = 0; i < aiBoard.length; i++) {
-			for (int j = 0; j < aiBoard[i].length; j++) {
-				int randGenSize = spinner.size();
-				double rand = Math.random();
-				int roll = (int) (randGenSize * rand);
-				aiBoard[i][j] = spinner.get(roll);
-				spinner.remove(roll);
-			}
-
-		}
-		
-		
-		*/
 		
 		for (int i = 0; i < aiBoolean.length; i++) {
 			for (int j = 0; j < aiBoolean[i].length; j++) {
@@ -122,7 +101,7 @@ public class BingoScreenSharon extends ClickableScreen implements Runnable {
 		for (int i = 0; i < playerBoard.length; i++) {
 			for (int j = 0; j < playerBoard[i].length; j++) {
 				int randGenSize = spinner.size();
-				int roll = randNumGenerator(randGenSize); // get a random index from current length to choose from
+				int roll = randNumGenerator(randGenSize-1); // get a random index from current length to choose from
 				
 				playerBoard[i][j] = spinner.get(roll);
 				spinner.remove(roll);
@@ -135,16 +114,26 @@ public class BingoScreenSharon extends ClickableScreen implements Runnable {
 			}
 		}
 	}
-
 	
-	public int randNumBallGenerator(int n) {
-		ArrayList<Integer> numBallList = new ArrayList<>();
-		Random random = new Random();
-
-		for (int i = 0; i < n; i++) {
-			numBallList.add(random.nextInt(25));
+	public void initBingoCage(){ //add 50 balls to bingo cage
+		for (int i = 1; i < 51; i++) {
+			numBallList.add(i);
 		}
-		return numBallList.size() - 1;
+	}
+	
+	public int randNumBallGenerator() {//Once a number ball is called, it is removed from the bingo cage.
+		//ArrayList<Integer> numBallList = new ArrayList<>();
+		/*
+		for (int i = 1; i < 51; i++) {
+			numBallList.add(i);
+		}
+		*/
+		int numsLeft= numBallList.size();
+		int randomIndex= randNumGenerator(numsLeft-1);
+		
+		int spinResult= numBallList.get(randomIndex);
+		numBallList.remove(randomIndex);
+		return spinResult;
 	}
 
 	@Override
@@ -165,3 +154,4 @@ public class BingoScreenSharon extends ClickableScreen implements Runnable {
 	}
 
 }
+//comment
