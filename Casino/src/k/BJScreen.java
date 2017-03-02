eb44cd4c185ef1685e15e3125148db004c729408
Student 6 <Student 6@BT_5E7_01-PC>
2017-03-02 12:12:50 -0500

    updated

--- Casino/src/k/BJScreen.java
+++ Casino/src/k/BJScreen.java
@@ -1,8 +1,6 @@
 package k;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import gui.components.Action;
@@ -33,6 +31,10 @@ public class BJScreen extends ClickableScreen implements Runnable {
 	public Graphic card2;
 	private Graphic backCard1;
 	private Graphic backCard2;
+	private	Box	card1outline;
+	private Box	card2outline;
+	private Box back1outline;
+	private Box back2outline;
 	public int positionCount = 185;
 	public int nextRow = 200;
 	
@@ -54,20 +56,28 @@ public class BJScreen extends ClickableScreen implements Runnable {
 		deck1 = new Deck();
 		
 		card1 = new Graphic(100, 200, 60, 80, PlayerHand.hand.get(0).image1);
+		card1outline = new Box(100,200,60,80, Color.black, false);
 		card2 = new Graphic(185, 200, 60, 80, PlayerHand.hand.get(1).image1);
+		card2outline = new Box(185,200,60,80, Color.black, false);
 		backCard1 = new Graphic(600, 200, 60, 80, "resources/sampleImages/backofcard.png");
+		back1outline = new Box(600,200,60,80, Color.black, false);
 		backCard2 = new Graphic(630, 200, 60, 80, "resources/sampleImages/backofcard.png");
+		back2outline = new Box(630,200,60,80, Color.black, false);
 		viewObjects.add(card1);
+		viewObjects.add(card1outline);
 		viewObjects.add(card2);
+		viewObjects.add(card2outline);
 		viewObjects.add(backCard1);
+		viewObjects.add(back1outline);
 		viewObjects.add(backCard2);
+		viewObjects.add(back2outline);
 		
 		name = new Graphic(250, 50,	300, 100, "resources/sampleImages/BlackJack.png");
 		
 		PlayerHand.checkValue(); //adds the values of the first 2 cards
 
 		playerCT = new TextLabel(300, 50, 250, 300, "Your current total is " + PlayerHand.getPlayerTotal()); //text label to show the total
-		box = new Box(278, 318, 250, 40, Color.green);
+		box = new Box(278, 318, 250, 40, Color.green, true);
 
 		playerCT = new TextLabel(300, 50, 400, 300, "Your current total is " + PlayerHand.getPlayerTotal()); //text label to show the total
 		hit = new Button(20, 350,65,40,"Hit", Color.green, new Action() {
@@ -78,6 +88,7 @@ public class BJScreen extends ClickableScreen implements Runnable {
 						PlayerHand.hit();
 						positionCount += 85;
 						viewObjects.add(new Graphic(positionCount, nextRow, 60, 80, PlayerHand.nextCard().image1));
+						viewObjects.add(new Box(positionCount, nextRow, 60,80, Color.black, false));
 						checkPosition();
 						PlayerHand.checkValue();
 						if(PlayerHand.getPlayerTotal() > 21 && PlayerHand.get2ndTotal() > 21){
@@ -104,6 +115,7 @@ public class BJScreen extends ClickableScreen implements Runnable {
 		restart = new Button(700, 350, 75, 40, "Restart", Color.green, new Action() {
 			public void act(){
 				initAllObjects(viewObjects);
+				PlayerHand.setStandCall();
 			}
 		});
 		
